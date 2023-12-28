9aef136e89b33c9e7709bb54c41c10ad0d241d30
Ziduan Zhang <573452881@qq.com>
2023-12-27 23:30:00 -0700

    initial

---
+++ DialogSys.cs
@@ -0,0 +1,430 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogSys : MonoBehaviour
+{
+    [Header("TextFiles")]
+    public TextAsset[] textFile = new TextAsset[31];
+
+    [Header("TextObjects")]
+    public TMP_Text textLabel1;
+    public TMP_Text textLabel2;
+    public TMP_Text textLabel3;
+    public TMP_Text textLabelB;
+    public TMP_Text choiceTextLabel1;
+    public TMP_Text choiceTextLabel2;
+    public TMP_Text choiceTextLabel3;
+
+    [Header("GameObjects")]
+    public GameObject Character1, Character2;
+    public GameObject Character3;
+    public GameObject Dialog3;
+    public GameObject Dialog1, Dialog2, DialogB;
+    public GameObject DialogChoice;
+    public GameObject[] Pointer = new GameObject[3];
+
+    int index;
+    TextAsset currentTextFile;
+    public List<string> textList = new List<string>();
+    bool choiceON = false;
+    int currentChoice = 0;
+    
+
+    private void OnEnable()
+    {
+        index = 0;
+
+        clearAll();
+
+        setCurrentTextFile();
+        GetTextFromFile(currentTextFile);
+        displayNext();
+    }
+
+    private void OnDisable()
+    {
+        Game.Control.startNext();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !choiceON)
+        {
+            index++;
+            displayNext();
+        }
+
+        if (choiceON)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (currentChoice == 0) { return; }
+                choiceOff();
+            }
+
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                currentChoice += 1;
+                if (currentChoice > 3) { currentChoice = 1; }
+                Pointer[0].SetActive(false); Pointer[1].SetActive(false); Pointer[2].SetActive(false);
+                Pointer[currentChoice - 1].SetActive(true);
+            }
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                currentChoice -= 1;
+                if (currentChoice < 1) { currentChoice = 3; }
+                Pointer[0].SetActive(false); Pointer[1].SetActive(false); Pointer[2].SetActive(false);
+                Pointer[currentChoice - 1].SetActive(true);
+            }
+
+        }
+    }
+
+    public void setCurrentTextFile()
+    {
+        switch (Game.Control.currentStatus)
+        {
+            case "test1":
+                currentTextFile = textFile[0];
+                break;
+            case "test2":
+                currentTextFile = textFile[1];
+                break;
+            case "0-1":
+                currentTextFile = textFile[2];
+                break;
+            case "1-1":
+                currentTextFile = textFile[3];
+                break;
+            case "1-1-2":
+                currentTextFile = textFile[4];
+                break;
+            case "1-2":
+                currentTextFile = textFile[5];
+                break;
+            case "1-3-1":
+                currentTextFile = textFile[6];
+                break;
+            case "1-3-2":
+                currentTextFile = textFile[7];
+                break;
+            case "1-3-3":
+                currentTextFile = textFile[8];
+                break;
+            case "2-1":
+                currentTextFile = textFile[9];
+                break;
+            case "2-2":
+                currentTextFile = textFile[10];
+                break;
+            case "2-3-A":
+                currentTextFile = textFile[11];
+                break;
+            case "2-3-B":
+                currentTextFile = textFile[12];
+                break;
+            case "2-4-1":
+                currentTextFile = textFile[13];
+                break;
+            case "2-4-2":
+                currentTextFile = textFile[14];
+                break;
+            case "2-4-3":
+                currentTextFile = textFile[15];
+                break;
+            case "2-4-4":
+                currentTextFile = textFile[16];
+                break;
+            case "2-4-5":
+                currentTextFile = textFile[17];
+                break;
+            case "END-0":
+                currentTextFile = textFile[18];
+                break;
+            case "END-A-1":
+                currentTextFile = textFile[19];
+                break;
+            case "END-A-2":
+                currentTextFile = textFile[20];
+                break;
+            case "END-A-3":
+                currentTextFile = textFile[21];
+                break;
+            case "END-A-4":
+                currentTextFile = textFile[22];
+                break;
+            case "END-B-1":
+                currentTextFile = textFile[23];
+                break;
+            case "END-B-2":
+                currentTextFile = textFile[24];
+                break;
+            case "END-B-3":
+                currentTextFile = textFile[25];
+                break;
+            case "END-B-4":
+                currentTextFile = textFile[26];
+                break;
+            case "END-B-5":
+                currentTextFile = textFile[27];
+                break;
+            case "END-C-1":
+                currentTextFile = textFile[28];
+                break;
+            case "END-C-2":
+                currentTextFile = textFile[29];
+                break;
+            case "END-C-3":
+                currentTextFile = textFile[30];
+                break;
+            case "END-C-4":
+                currentTextFile = textFile[31];
+                break;
+
+
+            default:
+                Debug.Log("ERROR_currentStatus_Undefine");
+                break;
+        }
+    }
+
+    void GetTextFromFile(TextAsset file)
+    {
+        textList.Clear();
+        index = 0;
+
+        var lineData = file.text.Split('\n');
+
+        foreach (var line in lineData)
+        {
+            textList.Add(line.Replace("\\n", ""));
+        }
+    }
+
+    void displayNext()
+    {
+        var status = false;
+        switch (textList[index])
+        {
+            case "&wait":
+                status = true;
+                break;
+            case "&end": 
+                dialogEnd();
+                break;
+            case "&clear":
+                clearAll();
+                break;
+
+            case "&c1_on":
+                Character1.SetActive(true);
+                break;
+            case "&c1_off":
+                Character1.SetActive(false);
+                break;
+            case "&c2_on":
+                Character2.SetActive(true);
+                break;
+            case "&c2_off":
+                Character2.SetActive(false);
+                break;
+            case "&c3_on":
+                Character3.SetActive(true);
+                break;
+            case "&c3_off":
+                Character3.SetActive(false);
+                break;
+            case "&d1_on":
+                Dialog1.SetActive(true);
+                break;
+            case "&d1_off":
+                Dialog1.SetActive(false);
+                break;
+            case "&d2_on":
+                Dialog2.SetActive(true);
+                break;
+            case "&d2_off":
+                Dialog2.SetActive(false);
+                break;
+            case "&d3_on":
+                Dialog3.SetActive(true);
+                break;
+            case "&d3_off":
+                Dialog3.SetActive(false);
+                break;
+            case "&db_on":
+                DialogB.SetActive(true);
+                break;
+            case "&db_off":
+                DialogB.SetActive(false);
+                break;
+
+            case "&choice_on":
+                choiceOn();
+                status = true;
+                break;
+            case "&save_choice":
+                Game.Control.savedAnswer = currentChoice;
+                break;
+
+            case "&blackin":
+                Game.Control.blackin();
+                break;
+            case "&blackout":
+                Game.Control.blackout();
+                break;
+            case "&oldpotato":
+                Game.Control.potato_old();
+                break;
+            case "&bgm_off":
+                Game.Control.noBGM();
+                break;
+
+            default:
+                var line = textList[index].Split(':');
+                switch (line[0])
+                {
+                    case "&bg_update":
+                        Game.Control.updateBG(int.Parse(line[1]));
+                        if (int.Parse(line[1]) > 9)
+                        {
+                            DialogB.GetComponent<Image>().sprite = null;
+                            DialogB.GetComponent<Image>().color = new Color32(0, 0, 0, 150);
+                            textLabelB.GetComponent<TMP_Text>().color = new Color32(255, 255, 255, 255);
+                        }
+                        break;
+                    case "&bgm":
+                        Game.Control.updateBGM(int.Parse(line[1]));
+                        break;
+                    case "&delay":
+                        status = true;
+                        StartCoroutine(delay(int.Parse(line[1])));
+                        break;
+
+                    case "d1":
+                        textLabel1.SetText(line[1]);
+                        break;
+                    case "d2":
+                        textLabel2.SetText(line[1]);
+                        break;
+                    case "d3":
+                        textLabel3.SetText(line[1]);
+                        break;
+                    case "db":
+                        textLabelB.SetText(line[1]);
+                        break;
+                    case "c1":
+                        choiceTextLabel1.SetText(line[1]);
+                        break;
+                    case "c2":
+                        choiceTextLabel2.SetText(line[1]);
+                        break;
+                    case "c3":
+                        choiceTextLabel3.SetText(line[1]);
+                        break;
+                    case "d2_choice_ans":
+                        switch (currentChoice)
+                        {
+                            case 0:
+                                textLabel2.SetText(line[1]);
+                                break;
+                            case 1:
+                                textLabel2.SetText(line[2]);
+                                break;
+                            case 2:
+                                textLabel2.SetText(line[3]);
+                                break;
+                            case 3:
+                                textLabel2.SetText(line[4]);
+                                break;
+                        }
+                        break;
+                }
+                break;
+        }
+
+        if (status) { return; }
+        index++;
+        displayNext();
+
+    }
+
+    void choiceOn()
+    {
+        Dialog1.SetActive(false);
+        textLabel1.SetText("");
+        currentChoice = 0;
+        Pointer[0].SetActive(false); Pointer[1].SetActive(false); Pointer[2].SetActive(false);
+        DialogChoice.SetActive(true);
+        choiceON = true;
+    }
+
+    void choiceOff()
+    {
+        DialogChoice.SetActive(false);
+        choiceON = false;
+        index++;
+        displayNext();
+    }
+
+    void dialogEnd()
+    {
+        index = 0;
+        clearAll();
+        gameObject.SetActive(false);
+    }
+
+    void clearAll()
+    {
+        //textList.Clear();
+        Character1.SetActive(false);
+        Character2.SetActive(false);
+        Character3.SetActive(false);
+        Dialog1.SetActive(false);
+        Dialog2.SetActive(false);
+        Dialog3.SetActive(false);
+        DialogB.SetActive(false);
+        DialogChoice.SetActive(false);
+    }
+
+    IEnumerator delay(int time)
+    {
+        yield return new WaitForSeconds(time);
+
+        index++;
+        //status = false;
+        displayNext();
+    }
+
+    /** ControlReference
+     * &c1_on
+     * &c1_off
+     * &c2_on
+     * &c2_off
+     * &d1_on
+     * &d1_off
+     * &d2_on
+     * &d2_off
+     * &db_on
+     * &db_off
+     * d1:[content]
+     * d2:[content]
+     * db:[content]
+     * c1:[content]
+     * c2:[content]
+     * c3:[content]
+     * &choice_on
+     * &save_choice
+     * d2_choice_ans:[ans0]:[ans1]:[ans2]:[ans3]
+     * &wait
+     * &delay:[time(s)]
+     * &blackin
+     * &blackout
+     * &bg_update:[index]
+     * &end
+     */
+
+}
---
+++ Game.cs
@@ -0,0 +1,374 @@
+using System.Collections;
+using System.Collections.Generic;
+//using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.Universal;
+using TMPro;
+
+public class Game : MonoBehaviour
+{
+    public static Game Control;
+
+    [Header("FlowControl")]
+    public string currentStatus = "undefine";
+
+    [Header("PlayerStatus")]
+    public int Score_water = 0;
+    public int Score_health = 100;
+    public float Score_nutrition = 0;
+
+    [Header("DialogSys")]
+    public GameObject dialogSys;
+    public int savedAnswer = -1;
+
+    [Header("GameSys")]
+    public bool canControl = false;
+    public GameObject gameSys_01;
+    public GameObject gameSys_02;
+    public GameObject gameSys_03;
+
+    [Header("GameObject")]
+    public Light2D GlobalLight;
+    public GameObject background;
+    public Sprite[] backgroundSprite = new Sprite[9];
+    public GameObject blackScreen;
+    public GameObject oldPotato;
+    public Sprite oldoldPotato;
+    public GameObject oldPotatoDialog;
+    public Sprite oldoldPotatoDialog;
+    public GameObject ending;
+
+    [Header("BGMPlayer")]
+    AudioSource BGM;
+    public AudioClip[] BGMList = new AudioClip[6];
+
+    private void Awake()
+    {
+        if (Control == null)
+        {
+            Control = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        BGM = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        startDialog("0-1");
+
+        //test
+        //startDialog("END-0");
+        //startGame("1-1-2G", 2);
+    }
+
+    public void startDialog(string status)
+    {
+        currentStatus = status;
+        dialogSys.SetActive(true);
+    }
+
+    public void startGame(string status,int gameNum)
+    {
+        currentStatus = status;
+        switch (gameNum)
+        {
+            case 1:
+                gameSys_01.SetActive(true);
+                break;
+            case 2:
+                gameSys_02.SetActive(true);
+                break;
+            case 3:
+                gameSys_03.SetActive(true);
+                break;
+
+            default:
+                Debug.Log("ERROR_gameSys_Number_Undefine");
+                break;
+        }
+        
+    }
+
+    public void startNext()
+    {
+        StartCoroutine(startNext_C());
+    }
+
+    IEnumerator startNext_C()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        switch (currentStatus)
+        {
+            case "0-1":
+                startDialog("1-1");
+                break;
+            case "1-1":
+                startGame("1-1G",1);
+                break;
+            case "1-1G":
+                startDialog("1-1-2");
+                break;
+            case "1-1-2":
+                startGame("1-1-2G", 2);
+                break;
+            case "1-1-2G":
+                startDialog("1-2");
+                break;
+            case "1-2":
+                startDialog("1-3-1");
+                break;
+            case "1-3-1":
+                startGame("1-3-1G", 1);
+                break;
+            case "1-3-1G":
+                startDialog("1-3-2");
+                break;
+            case "1-3-2":
+                startGame("1-3-2G", 2);
+                break;
+            case "1-3-2G":
+                startDialog("1-3-3");
+                break;
+            case "1-3-3":
+                startGame("1-3-3G", 1);
+                break;
+            case "1-3-3G":
+                startDialog("2-1");
+                break;
+            case "2-1":
+                startGame("2-1G", 3);
+                break;
+            case "2-1G":
+                startDialog("2-2");
+                break;
+            case "2-2":
+                if (savedAnswer == 1)
+                {
+                    startDialog("2-3-A");
+                }
+                else
+                {
+                    startDialog("2-3-B");
+                }
+                break;
+            case "2-3-A":
+            case "2-3-B":
+                startDialog("2-4-1");
+                break;
+            case "2-4-1":
+                startGame("2-4-1G", 2);
+                break;
+            case "2-4-1G":
+                startDialog("2-4-2");
+                break;
+            case "2-4-2":
+                startGame("2-4-2G", 3);
+                break;
+            case "2-4-2G":
+                startDialog("2-4-3");
+                break;
+            case "2-4-3":
+                startGame("2-4-3G", 1);
+                break;
+            case "2-4-3G":
+                startDialog("2-4-4");
+                break;
+            case "2-4-4":
+                startGame("2-4-4G", 2);
+                break;
+            case "2-4-4G":
+                startDialog("2-4-5");
+                break;
+            case "2-4-5":
+                startGame("2-4-5G", 3);
+                break;
+
+            ////for"3-0-Test"only
+            //case "2-4-5G":
+            //    startDialog("3-0-Test");
+            //    showData();
+            //    break;
+
+            case "2-4-5G":
+                startDialog("END-0");
+                break;
+
+            case "END-0":
+                switch (savedAnswer)
+                {
+                    case 1:
+                        if (Score_water >= 300 && Score_nutrition >= 85 && Score_health >= 75)
+                        {
+                            startDialog("END-A-1");
+                        }
+                        else
+                        {
+                            if (Score_water >= 280 && Score_nutrition >= 80 && Score_health >= 70)
+                            {
+                                startDialog("END-A-2");
+                            }
+                            else
+                            {
+                                if (Score_water >= 200 && Score_health >= 60)
+                                {
+                                    startDialog("END-A-3");
+                                }
+                                else
+                                {
+                                    startDialog("END-A-4");
+                                }
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        if (Score_water >= 300 && Score_nutrition >= 85 && Score_health >= 75)
+                        {
+                            startDialog("END-B-1");
+                        }
+                        else
+                        {
+                            if (Score_water >= 280 && Score_nutrition >= 80 && Score_health >= 70)
+                            {
+                                startDialog("END-B-2");
+                            }
+                            else
+                            {
+                                if (Score_water >= 250 && Score_nutrition >= 80 && Score_health >= 65)
+                                {
+                                    startDialog("END-B-3");
+                                }
+                                else
+                                {
+                                    if (Score_water >= 200 && Score_health >= 60)
+                                    {
+                                        startDialog("END-B-4");
+                                    }
+                                    else
+                                    {
+                                        startDialog("END-B-5");
+                                    }
+                                }
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        if (Score_water >= 280 && Score_nutrition >= 85 && Score_health >= 70)
+                        {
+                            startDialog("END-C-1");
+                        }
+                        else
+                        {
+                            if (Score_water >= 250 && Score_nutrition >= 80 && Score_health >= 65)
+                            {
+                                startDialog("END-C-2");
+                            }
+                            else
+                            {
+                                if (Score_water >= 230 && Score_health >= 60)
+                                {
+                                    startDialog("END-C-3");
+                                }
+                                else
+                                {
+                                    startDialog("END-C-4");
+                                }
+                            }
+                        }
+                        break;
+
+                    default:
+                        Debug.Log("ERROR_Not_valid_saved_choice");
+                        break;
+                }
+                break;
+
+            case "END-A-1":
+            case "END-A-2":
+            case "END-A-3":
+            case "END-A-4":
+            case "END-B-1":
+            case "END-B-2":
+            case "END-B-3":
+            case "END-B-4":
+            case "END-B-5":
+            case "END-C-1":
+            case "END-C-2":
+            case "END-C-3":
+            case "END-C-4":
+                ending.SetActive(true);
+                break;
+
+            default:
+                Debug.Log("ERROR_startNext_Status_Undefine");
+                break;
+        }
+    }
+
+    public void updateBG(int bg_index)
+    {
+        background.GetComponent<SpriteRenderer>().sprite = backgroundSprite[bg_index-1];
+    }
+
+    public void updateBGM(int bgm_index)
+    {
+        StartCoroutine(Weak(BGM, bgm_index));
+    }
+
+    public void noBGM()
+    {
+        StartCoroutine(NoBGM(BGM));
+    }
+
+    IEnumerator Weak(AudioSource bgm, int index)
+    {
+        while (bgm.volume > 0.02f)
+        {
+            bgm.volume -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        } 
+        bgm.clip = BGMList[index - 1]; 
+        bgm.Play(); // or it won't play
+        while (bgm.volume < 1f)
+        {
+            bgm.volume += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator NoBGM(AudioSource bgm)
+    {
+        while (bgm.volume > 0.01f)
+        {
+            bgm.volume -= 0.01f;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    public void blackin()
+    {
+        blackScreen.GetComponent<Animator>().SetBool("blackout", false);
+        blackScreen.GetComponent<Animator>().SetBool("blackin", true);
+    }
+
+    public void blackout()
+    {
+        blackScreen.GetComponent<Animator>().SetBool("blackin", false);
+        blackScreen.GetComponent<Animator>().SetBool("blackout", true);
+    }
+
+    public void potato_old()
+    {
+        oldPotato.GetComponent<Image>().sprite = oldoldPotato;
+        oldPotatoDialog.GetComponent<Image>().sprite = oldoldPotatoDialog;
+    }
+
+}
